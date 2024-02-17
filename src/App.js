2a6945d22fabc4206ea6b21aaab88cdbd3cab397
Joshua Richter <joshuawiidrichter@gmail.com>
2024-02-17 17:46:25 +0200

    Added sections/styling/links

--- src/App.js
+++ src/App.js
@@ -3,23 +3,50 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <h1>PURRFECTION</h1>
-      <img className="background-img" src={require('./images/cover.jpg')} />
-      <div className="socials-container">
+      <div className="section-main">
+        <h1>PURRFECTION</h1>
+        <img className="background-img" src={require('./images/cover.jpg')} />
+      </div>
+      <h2>RELEASES</h2>
+      <div className="section-releases">
         <a
-          href="https://open.spotify.com/artist/25kA7PpCncWzjXu8eUH1YM?si=zPvR_h9KR_e6UHGKfgGcfw"
-          className="social-link"
+          href="https://open.spotify.com/track/69kK1B7K5S5cnQZANjxJwN?si=72708b96b63e43f5"
           target="_blank"
         >
-          <img src={require('./images/spotify.png')} />
+          <div className="description-container">
+            <p className="text">It's Alright</p>
+          </div>
         </a>
         <a
-          href="https://www.youtube.com/channel/UCoILuJsgXlw8xKLViNAtkpg"
-          className="social-link"
+          href="https://open.spotify.com/track/69kK1B7K5S5cnQZANjxJwN?si=72708b96b63e43f5"
           target="_blank"
         >
-          <img src={require('./images/youtube.png')} />
+          <div className="image-container">
+            <img
+              className="release-img"
+              src={require('./images/itsAlright.png')}
+            />
+          </div>
         </a>
+      </div>
+      <h2>SOCIALS</h2>
+      <div className="section-footer">
+        <div className="socials-container">
+          <a
+            href="https://open.spotify.com/artist/25kA7PpCncWzjXu8eUH1YM?si=zPvR_h9KR_e6UHGKfgGcfw"
+            className="social-link"
+            target="_blank"
+          >
+            <img src={require('./images/spotify.png')} />
+          </a>
+          <a
+            href="https://www.youtube.com/channel/UCoILuJsgXlw8xKLViNAtkpg"
+            className="social-link"
+            target="_blank"
+          >
+            <img src={require('./images/youtube.png')} />
+          </a>
+        </div>
       </div>
     </div>
   );
