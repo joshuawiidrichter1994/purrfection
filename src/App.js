c1c4140c0155bd112e13d4dc55977632d6b60fa5
Joshua Richter <joshuawiidrichter@gmail.com>
2024-02-17 17:07:47 +0200

    Added images / Removed boilerplate / Styled landing page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <img className="background-img" src={require('./images/cover.jpg')} />
+    </div>
+  );
 }
 
 export default App;
